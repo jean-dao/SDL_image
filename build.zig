68a08edbf884fe6a832231e54faceb115ae84242
Jean Dao <jean@pfudke.fr>
2023-12-01 18:35:30 +0100

    update to work with latest andrewrk/SDL and zig-0.12.0-dev.890+8c6b0271c

--- build.zig
+++ build.zig
@@ -10,10 +10,9 @@ pub fn build(b: *std.build.Builder) void {
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     });
-    
 
     lib.linkLibC();
-    
+
     lib.defineCMacro("USE_STBIMAGE", "1");
     lib.defineCMacro("LOAD_BMP", "1");
     lib.defineCMacro("LOAD_GIF", "1");
@@ -21,34 +20,37 @@ pub fn build(b: *std.build.Builder) void {
     lib.defineCMacro("LOAD_PNG", "1");
     lib.defineCMacro("LOAD_SVG", "1");
     lib.defineCMacro("LOAD_TGA", "1");
-    
+
     lib.linkLibrary(libSDL);
 
-    lib.addCSourceFiles(&.{
-        "IMG_avif.c",
-        "IMG_bmp.c",
-        "IMG_gif.c",
-        "IMG_jpg.c",
-        "IMG_jxl.c",
-        "IMG_lbm.c",
-        "IMG_pcx.c",
-        "IMG_png.c",
-        "IMG_pnm.c",
-        "IMG_qoi.c",
-        "IMG_stb.c",
-        "IMG_svg.c",
-        "IMG_tga.c",
-        "IMG_tif.c",
-        "IMG_webp.c",
-        "IMG_WIC.c",
-        "IMG_xcf.c",
-        "IMG_xpm.c",
-        "IMG_xv.c",
-        "IMG_xxx.c",
-        "IMG.c",
-    }, &.{
-        "-std=c99",
+    lib.addCSourceFiles(.{
+        .files = &.{
+            "src/IMG_avif.c",
+            "src/IMG_bmp.c",
+            "src/IMG_gif.c",
+            "src/IMG_jpg.c",
+            "src/IMG_jxl.c",
+            "src/IMG_lbm.c",
+            "src/IMG_pcx.c",
+            "src/IMG_png.c",
+            "src/IMG_pnm.c",
+            "src/IMG_qoi.c",
+            "src/IMG_stb.c",
+            "src/IMG_svg.c",
+            "src/IMG_tga.c",
+            "src/IMG_tif.c",
+            "src/IMG_webp.c",
+            "src/IMG_WIC.c",
+            "src/IMG_xcf.c",
+            "src/IMG_xpm.c",
+            "src/IMG_xv.c",
+            "src/IMG_xxx.c",
+            "src/IMG.c",
+        },
+        .flags = &.{
+            "-std=c99",
+        },
     });
-    lib.installHeader("SDL_image.h", "SDL2/SDL_image.h");
+    lib.installHeader("src/SDL_image.h", "SDL2/SDL_image.h");
     b.installArtifact(lib);
 }
